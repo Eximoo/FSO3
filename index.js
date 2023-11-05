abba44c36190e1b98e3ae2ef42493aae51ab2419
exi <eximo888@gmail.com>
2023-11-05 17:29:40 +0100

    Phonebook database, step3

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+require('dotenv').config();
 app.use(express.json());
 app.use(express.static(__dirname + '/dist'));
-// const cors = require('cors');
-// app.use(cors());
-
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
+const Person = require('./models/person');
 const persons = [
   {
     id: 1,
@@ -35,7 +34,10 @@ const persons = [
 ];
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+  // response.json(persons);
 });
 
 app.get('/info', (request, response) => {
@@ -44,26 +46,23 @@ app.get('/info', (request, response) => {
   );
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const target = persons.find((person) => person.id == Number(id));
-  if (target) {
-    response.json(target);
-  } else {
-    response.statusMessage = 'Person was not found';
-    response.status(404).send('Person Not found');
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 app.get('/index.html', (request, response) => {
   response.sendFile('./dist/index.html');
 });
+
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const index = persons.findIndex((person) => person.id === Number(id));
-  if (index != -1) {
-    persons.splice(index, 1);
-  }
-  response.status(204).send();
+  Person.findByIdAndDelete(request.params.id).then(response.status(204).send());
 });
 app.post('/api/persons', (request, response) => {
   if (!request.body.name || !request.body.number) {
@@ -83,8 +82,14 @@ app.post('/api/persons', (request, response) => {
 // };
 
 // app.use(unknownEndpoint);
-
-const PORT = process.env.PORT || 3001;
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+app.use(errorHandler);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
