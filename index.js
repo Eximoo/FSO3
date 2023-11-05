8530920628aaa909ad7d202d88f5c683e5871047
exi <eximo888@gmail.com>
2023-11-05 17:54:08 +0100

    fix for Phonebook database, step2

--- index.js
+++ index.js
@@ -10,34 +10,11 @@ app.use(
 );
 
 const Person = require('./models/person');
-const persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
   });
-  // response.json(persons);
 });
 
 app.get('/info', (request, response) => {
@@ -64,24 +41,16 @@ app.get('/index.html', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id).then(response.status(204).send());
 });
-app.post('/api/persons', (request, response) => {
-  if (!request.body.name || !request.body.number) {
-    return response.status(400).send({ error: 'All fields are required' });
-  }
-  if (persons.find((person) => person.name === request.body.name)) {
-    return response
-      .status(400)
-      .send({ error: 'Person with that already exists' });
-  }
-  persons.push({ id: Math.floor(Math.random() * 1000000000), ...request.body });
-  response.send(request.body);
+
+app.put('/api/persons', (request, response) => {
+  Person.create(request.body).then((result) => response.send(result));
 });
 
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' });
-// };
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-// app.use(unknownEndpoint);
+app.use(unknownEndpoint);
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
