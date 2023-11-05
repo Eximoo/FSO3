acc4819dcaf4f392a73b1d7272db423235e29b4d
exi <eximo888@gmail.com>
2023-11-05 18:19:11 +0100

    Phonebook database, step3

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
